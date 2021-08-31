13b6c7337f1b4840a0d57a3b3cd8aaa60f6f0bc1
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-08-31 23:28:59 +0300

    lab28 done

--- app/src/main/java/com/example/taskmaster/TaskDetail.java
+++ app/src/main/java/com/example/taskmaster/TaskDetail.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.lang.String.valueOf;
 
 public class TaskDetail extends AppCompatActivity {
 
@@ -21,7 +25,29 @@ public class TaskDetail extends AppCompatActivity {
         super.onResume();
         Intent intent=getIntent();
         String task=intent.getExtras().getString("task");
+        String taskTitle=intent.getExtras().getString("title");
+        String taskStateSt=intent.getExtras().getString("state");
+        String taskBodySt=intent.getExtras().getString("body");
         TextView TaskName=findViewById(R.id.TaskDetailTitle);
+        TextView taskBody=findViewById(R.id.TaskDetailBody);
+        TextView taskState=findViewById(R.id.TaskDetailState);
+        boolean test=taskTitle.isEmpty();
+//        String msg=valueOf(test);
+//        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG);
+        if (taskTitle=="" && taskStateSt==""&& taskTitle==""){
             TaskName.setText(task);
+
+        }else {
+
+            TaskName.setText(taskTitle);
+            taskBody.setText(taskBodySt);
+            taskState.setText(taskStateSt);
+        }
+        Button goHome=findViewById(R.id.TaskDetailHomeButton);
+        goHome.setOnClickListener(view -> {
+            Intent intent1=new Intent(TaskDetail.this,MainActivity.class);
+            startActivity(intent1);
+        });
+
     }
 }
